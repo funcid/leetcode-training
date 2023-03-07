1c0edde91e5508591c6401273c67e0b4943a225a
Артем Царюк <rtem903134@gmail.com>
2023-03-07 21:54:55 +0300

    Create 9. Palindrome Number.java

---
+++ 9. Palindrome Number.java
@@ -0,0 +1,15 @@
+class Solution {
+	public boolean isPalindrome(int x) {
+
+    		if (x < 0 || (x != 0 && x % 10 == 0)) return false;
+
+    		int reversed = 0;
+
+    		while (x > reversed) {
+    			reversed = reversed * 10 + x % 10;
+    			x = x / 10;
+    		}
+
+    		return (x == rev || x == reversed / 10);
+	}
+}
